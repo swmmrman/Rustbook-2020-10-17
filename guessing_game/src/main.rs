db979e0e1dfb7a35d0e4cf454d4b494f34961974
Stephan Kern <stephan@swmmrman.com>
2020-10-25 18:24:15 -0600

    Remove tell, and unneed lines.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,15 +4,10 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
-
     let secret_number = rand::thread_rng().gen_range(1,101);
-
-    println!("The secret number is: {}", secret_number);
-
     loop {
         println!("Please input your guess.");
         let mut guess = String::new();
-
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
@@ -24,9 +19,7 @@ fn main() {
                 continue;
             }
         };
-
         println!("You guessed : {}", guess);
-
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Small Thoughts?"),
             Ordering::Greater => println!("Dreaming big?"),
