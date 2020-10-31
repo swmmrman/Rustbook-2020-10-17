032c49ab23f304b653150ec2a8161ca9eb16029f
Stephan Kern <stephan@swmmrman.com>
2020-10-31 14:57:09 -0600

    1 second sleep

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     while number & 0b10000000000000000000000000000000 == 0 {
         println!("{:064b}!", number);
         number -= 1;
+        let wait = std::time::Duration::from_secs(1);
+        std::thread::sleep(wait);
     }
     println!("Liftoff!!!");
 }
