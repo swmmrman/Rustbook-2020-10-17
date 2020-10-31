bfc850098cbc165ad7ba8e6eda27628d66111e4b
Stephan Kern <stephan@swmmrman.com>
2020-10-31 14:49:53 -0600

    Binary countdown now

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let mut number = 0x000000F;
+    let mut number = 0x000001F;
 
     while number & 0b1000000000000000 == 0 {
-        println!("{}!", number);
+        println!("{:08b}!", number);
         number -= 1;
     }
     println!("Liftoff!!!");
