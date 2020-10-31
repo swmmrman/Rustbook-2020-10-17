7eff136785979e4cd18bce4d5c28fb311b0f1a89
Stephan Kern <stephan@swmmrman.com>
2020-10-31 14:53:51 -0600

    64bit version.

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let mut number = 0x000001F;
+    let mut number: i64 = 0x00000000000003F;
 
-    while number & 0b1000000000000000 == 0 {
-        println!("{:032b}!", number);
+    while number & 0b10000000000000000000000000000000 == 0 {
+        println!("{:064b}!", number);
         number -= 1;
     }
     println!("Liftoff!!!");
