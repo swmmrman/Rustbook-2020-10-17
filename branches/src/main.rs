ead8068da9a3d487f799778b7bf422e00af27a5d
Stephan Kern <stephan@swmmrman.com>
2020-10-31 13:46:32 -0600

    3.5 Using if in a let.  Spread over multiple lines

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,16 +1,30 @@
 fn main() {
-    let number = 6;
+    let condition = true;
+    let number = if condition {
+        5
+    } else {
+        "Six"
+    };
+//
+//     Compiling branches v0.1.0 (/media/LinuxStorageCryp/MyCurrentStorage/Coding/Rustbook-2020-10-17/branches)
+// error[E0308]: `if` and `else` have incompatible types
+// --> src/main.rs:20:9
+//    |
+// 17 |       let number = if condition {
+//    |  __________________-
+// 18 | |         5
+//    | |         - expected because of this
+// 19 | |     } else {
+// 20 | |         "Six"
+//    | |         ^^^^^ expected integer, found `&str`
+// 21 | |     };
+//    | |_____- `if` and `else` have incompatible types
+//
+// error: aborting due to previous error
+//
+// For more information about this error, try `rustc --explain E0308`.
+// error: could not compile `branches`.
+//
+// To learn more, run the command again with --verbose.
 
-    if number % 4 == 0 {
-        println!("The number was divisible by 4.");
-    }
-    else if number % 3 == 0 {
-        println!("The number was divisible by 3.");
-    }
-    else if number % 2 == 0 {
-        println!("The number was divisible by 2.");
-    }
-    else {
-        println!("The number was not divisible by 4,3, or 2.");
-    }
 }
