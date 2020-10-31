056a524709da3e50c57001ecb6a848c9f9236962
Stephan Kern <stephan@swmmrman.com>
2020-10-31 13:24:29 -0600

    3.5.1

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 3;
+
+    if number < 5 {
+        println!("Condition was true");
+    }
+    else {
+        println!("Condition was false");
+    }
 }
