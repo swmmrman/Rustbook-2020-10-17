05e6b4557e38a41652b358a9ae52498a2860d03b
Stephan Kern <stephan@swmmrman.com>
2020-10-31 14:46:09 -0600

    Having fun with condtional loops.  Comparing in binary looking for -1

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let mut number = 3;
+    let mut number = 0x000000F;
 
-    while number != 0 {
+    while number & 0b1000000000000000 == 0 {
         println!("{}!", number);
         number -= 1;
     }
