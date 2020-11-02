c53b5daed60049feebc7cd1c38d0e92f879d40e9
Stephan Kern <stephan@swmmrman.com>
2020-11-01 19:34:47 -0700

    First temp convertor working f -> c

--- CH3-5-Exercises/temp_convertor/src/main.rs
+++ CH3-5-Exercises/temp_convertor/src/main.rs
@@ -1,3 +1,9 @@
+fn convert_to_c(temp_f:f32) -> f32{
+    (temp_f - 32.0) * 5.0/9.0
+}
+
 fn main() {
-    
+    let temp_f = 212.0;
+    let temp_c = convert_to_c(temp_f);
+    println!("{}f is {}c", temp_f, temp_c)
 }
