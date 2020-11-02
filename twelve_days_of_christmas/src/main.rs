84d4f37ca2e0d3efad47a11ea68221b253b14b12
Stephan Kern <stephan@swmmrman.com>
2020-11-01 23:47:43 -0700

    Add more days and refactor

--- twelve_days_of_christmas/src/main.rs
+++ twelve_days_of_christmas/src/main.rs
@@ -1,19 +1,25 @@
 fn do_day(day: i32) {
-    println!("On the {} of Christmas my true love gave to me", day);
+    println!("On the {} of Christmas my true love gave to me:", day);
     let mut out: String = "".to_string();
     for count in (1..(day+1)).rev() {
-        match count {
-            1 => out.push_str("A partidge in a pear tree."),
-            2 => out.push_str("2 turtle doves, and\n"),
-            3 => out.push_str("3 French Hens\n"),
-            _ => println!("WTF"),
-        }
+        let text = match count {
+            1 => "A partidge in a pear tree.",
+            2 => "2 turtle doves, and",
+            3 => "3 French Hens",
+            4 => "4 Calling Birds",
+            5 => "5 Golden Ring",
+            6 => "6 Geese a-laying",
+            7 => "7 Sawns a-swimming",
+            _ => "Invalid day, Bonk to the head.",
+        };
+        out.push_str(text);
+        out.push_str("\n");
     }
-    println!("{}\n",out);
+    println!("{}",out);
 }
 
 fn main() {
-    for day in 1..4 {
+    for day in 1..13 {
         do_day(day);
     }
 }
