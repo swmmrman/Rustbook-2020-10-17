8db7b9fa22cc622763795e26cf078b6e09431136
Stephan Kern <stephan@swmmrman.com>
2020-11-01 23:17:19 -0700

    Working but missing days

--- twelve_days_of_christmas/src/main.rs
+++ twelve_days_of_christmas/src/main.rs
@@ -1,3 +1,19 @@
+fn do_day(day: i32) {
+    println!("On the {} of Christmas my true love gave to me", day);
+    let mut out: String = "".to_string();
+    for count in (1..(day+1)).rev() {
+        match count {
+            1 => out.push_str("A partidge in a pear tree."),
+            2 => out.push_str("2 turtle doves, and\n"),
+            3 => out.push_str("3 French Hens\n"),
+            _ => println!("WTF"),
+        }
+    }
+    println!("{}\n",out);
+}
+
 fn main() {
-    println!("Hello, world!");
+    for day in 1..4 {
+        do_day(day);
+    }
 }
