231852177b985c37e6c21fb4fa6327893342f507
Stephan Kern <stephan@swmmrman.com>
2020-11-01 19:21:39 -0700

    src/main.rs
    fibonacci first try

--- CH3-5-Exercises/fibonacci/src/main.rs
+++ CH3-5-Exercises/fibonacci/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let mut num = 1;
+    let mut prev_num = 0;
+
+    while num < 50 {
+        let new_num = prev_num + num;
+        prev_num = num;
+        num = new_num;
+        print!("{},", num);
+    }
 }
