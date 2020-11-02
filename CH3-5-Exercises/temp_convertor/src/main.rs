926a657a58d4b5f0db61e3b57c04a4e88d0a19e9
Stephan Kern <stephan@swmmrman.com>
2020-11-01 19:43:34 -0700

    Now works both ways

--- CH3-5-Exercises/temp_convertor/src/main.rs
+++ CH3-5-Exercises/temp_convertor/src/main.rs
@@ -2,8 +2,15 @@ fn convert_to_c(temp_f:f32) -> f32{
     (temp_f - 32.0) * 5.0/9.0
 }
 
+fn convert_to_f(temp_c:f32) -> f32 {
+    (temp_c * 9.0/5.0) + 32.0
+}
+
 fn main() {
     let temp_f = 212.0;
     let temp_c = convert_to_c(temp_f);
-    println!("{}f is {}c", temp_f, temp_c)
+    println!("{}f is {}c", temp_f, temp_c);
+    let temp_c = 100.0;
+    let temp_f = convert_to_f(temp_c);
+    println!("{}c is {}f", temp_c, temp_f);
 }
