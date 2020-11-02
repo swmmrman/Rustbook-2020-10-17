f0cde6227b14c97141f357d4b1a9b260cab44f5d
Stephan Kern <stephan@swmmrman.com>
2020-11-01 18:24:28 -0700

    More adjusting and using bitwise not to look for -1.  Cannot directly code bit 64 to 1 without overflow errors.

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -1,10 +1,10 @@
 fn main() {
-    let mut number: i64 = 0x00000000000003F;
+    let mut number: i64 = 0x7FFFFFFFFFFFFFFF;
 
-    while number & 0b10000000000000000000000000000000 == 0 {
+    while !number != 0 {
         println!("{:064b}!", number);
         number -= 1;
-        let wait = std::time::Duration::from_secs(1);
+        let wait = std::time::Duration::from_millis(1);
         std::thread::sleep(wait);
     }
     println!("Liftoff!!!");
