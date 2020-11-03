6abbc60dbff77ba209573943c6c23f7c8bc3295e
Stephan Kern <stephan@swmmrman.com>
2020-11-02 19:22:53 -0700

    Preparing for "Methods with more parameters"

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -16,12 +16,22 @@ impl Rectangle {
         self.width *= multi;
         self.height *= multi;
     }
+    // fn ca
 }
 
 fn main() {
-    let mut rect = Rectangle{
+    let mut rect1 = Rectangle {
         width:30,
         height:50,
     };
-
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
