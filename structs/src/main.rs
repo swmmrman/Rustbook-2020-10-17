58ab535e24cff2e8444161da2d6af4920fe14394
Stephan Kern <stephan@swmmrman.com>
2020-11-02 18:43:07 -0700

    5.2.1 with the function up top

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,12 @@
+fn area(width: u32, height: u32) -> u32 {
+    width * height
+}
+
 fn main() {
-    println!("Hello, world!");
+    let width1 = 30;
+    let height1 = 50;
+    println!(
+        "The area of the rectangle is {} pixels squared.",
+        area(width1, height1)
+    );
 }
