d62a36b3e9c07f6efcabdbbdcbf6321ec9e2d0b7
Stephan Kern <stephan@swmmrman.com>
2020-11-02 19:07:11 -0700

    Made the rectangle mutable and added a double sides method.  Not an exercise just fun

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -8,13 +8,22 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn double_sides(&mut self) {
+        self.width *= 2;
+        self.height *= 2;
+    }
 }
 
 fn main() {
-    let rect = Rectangle{
+    let mut rect = Rectangle{
         width:30,
         height:50,
     };
+    println!(
+        "The area of the rectangle is {} pixels squared.",
+        rect.area()
+    );
+    rect.double_sides();
     println!(
         "The area of the rectangle is {} pixels squared.",
         rect.area()
