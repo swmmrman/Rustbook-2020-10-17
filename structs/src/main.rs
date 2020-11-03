26c99d43e789bcdc83c7237a22dd56002b2da32a
Stephan Kern <stephan@swmmrman.com>
2020-11-02 19:29:42 -0700

    My attempt to make a can_hold method

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -16,7 +16,9 @@ impl Rectangle {
         self.width *= multi;
         self.height *= multi;
     }
-    // fn ca
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 
 fn main() {
