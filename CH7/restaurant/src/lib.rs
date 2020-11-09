6d4fc2a8088bac001da350bdfc762d975dddd743
Stephan Kern <stephan@swmmrman.com>
2020-11-09 01:06:43 -0700

    Up to listing 7-7.  Have not removed serving module.

--- CH7/restaurant/src/lib.rs
+++ CH7/restaurant/src/lib.rs
@@ -1,11 +1,17 @@
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
-        fn set_at_table() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
     }
     mod serving {
         fn take_order() {}
         fn serve_order() {}
         fn take_payment() {}
     }
+}
+pub fn eat_at_restaurant() {
+    // Absolute version
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative version
+    front_of_house::hosting::add_to_waitlist();1
 }
