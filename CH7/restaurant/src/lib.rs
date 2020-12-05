3879ec8c27a05ae2b759337846597da648103f39
Stephan Kern <stephan@swmmrman.com>
2020-12-04 20:10:07 -0700

    Matching 7-9, Comments reworded

--- CH7/restaurant/src/lib.rs
+++ CH7/restaurant/src/lib.rs
@@ -7,19 +7,34 @@ mod front_of_house {
         fn take_payment() {}
     }
 }
-pub fn eat_at_restaurant() {
-    // Absolute version
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // Relative version
-    front_of_house::hosting::add_to_waitlist();
-}
 
 fn serve_order() {}
 mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
     fn fix_incorrect_order() {
         cook_order();
         super::serve_order();
     }
     fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    // Order with Rye
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change mind to Wheat
+    meal.toast = String::from("Wheat");
+    println!("I'd link {} toast please", meal.toast);
+    // Next line will not compile, due to accesing a private field.
+    // meal.seasonal_fruit = String::from("blueberries");
 }
