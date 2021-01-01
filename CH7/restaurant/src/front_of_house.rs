5d4595a291e3c2e4417f7bd158c06f2d88da323f
Stephan Kern <stephan@swmmrman.com>
2021-01-01 16:34:55 -0700

    More module splitting

--- CH7/restaurant/src/front_of_house.rs
+++ CH7/restaurant/src/front_of_house.rs
@@ -1,7 +1,6 @@
+pub mod hosting;
+
 mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
     mod serving {
         fn take_order() {}
         fn take_payment() {}
