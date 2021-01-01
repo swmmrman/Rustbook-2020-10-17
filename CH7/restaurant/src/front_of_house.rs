692fd11e9f8d0ef4c36748f7d3186b00ed6484a0
Stephan Kern <stephan@swmmrman.com>
2021-01-01 16:30:23 -0700

    7-21 splitting front of house to its own module

---
+++ CH7/restaurant/src/front_of_house.rs
@@ -0,0 +1,9 @@
+mod front_of_house {
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
+    }
+    mod serving {
+        fn take_order() {}
+        fn take_payment() {}
+    }
+}
--- CH7/restaurant/src/lib.rs
+++ CH7/restaurant/src/lib.rs
@@ -1,12 +1,4 @@
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-    }
-    mod serving {
-        fn take_order() {}
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 fn serve_order() {}
 mod back_of_house {
