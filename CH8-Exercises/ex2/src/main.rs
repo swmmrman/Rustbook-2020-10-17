f8d69f5f54ba30a947388011cf0d082ed3ad14ca
Stephan Kern <stephan@swmmrman.com>
2021-02-25 03:01:25 -0700

    Working with hardcoded string

--- CH8-Exercises/ex2/src/main.rs
+++ CH8-Exercises/ex2/src/main.rs
@@ -1,3 +1,22 @@
+fn mutate(word: &str) -> String {
+    let vowels = vec!["a", "e", "i", "o", "u", "y"];
+    let small = word.to_lowercase();        // Change string to lowercase.
+    let firstletter = &small[0..1];         // Pull off first letter.
+    let results;                            // Decalre storage space
+    if vowels.contains(&firstletter) {      // add hay to ones starting in vowels.
+        results = format!("{}-hay", &word);
+    }
+    else {                                  // rearrange the others.
+        results = format!("{}-{}ay", &word[1..], firstletter);
+    }
+    results
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sentence = "I wrote a thing";
+    let mut newsentence = String::new();
+    for word in sentence.split(" ") {
+        newsentence = format!("{} {}",&newsentence, mutate(&word));
+    }
+    println!("{}", newsentence);
 }
