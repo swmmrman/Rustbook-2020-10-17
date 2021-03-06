0098f805c4f4b45c83ef745dc474d08b6a60adf8
Stephan Kern <stephan@swmmrman.com>
2021-03-06 12:41:45 -0700

    12.9 Found my own way to working

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,12 @@
-use std::env;
-use std::fs;
+use std::{env,fs,process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("{}", err);
+        process::exit(1);
+    });
     println!("Hunting for \"{}\" in {}", config.query, config.file_name);
 
     let text = fs::read_to_string(config.file_name)
@@ -21,14 +23,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, String> {
         if args.len() < 3 {
-            panic!("Usage: {} \"search term\" file_name", args[0])
+            return Err(format!("Usage: {} \"search term\" file_name", args[0]));
         }
         let _binary_name = args[0].clone();
         let query = args[1].clone();
         let file_name = args[2].clone();
 
-        Config {_binary_name, query, file_name}
+        Ok(Config {_binary_name, query, file_name})
     }
 }
