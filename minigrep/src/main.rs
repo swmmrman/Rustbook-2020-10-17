eed8a426e6c1e2ada35f1b63f273dfaff1eab2bf
Stephan Kern <stephan@swmmrman.com>
2021-03-06 12:13:12 -0700

    Changes up to 12.7 Changed filename to file_name

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,14 +4,26 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
+    println!("Hunting for \"{}\" in {}", config.query, config.file_name);
 
-    println!("Hunting for \"{}\" in {}", query, filename);
-
-    let text = fs::read_to_string(filename)
+    let text = fs::read_to_string(config.file_name)
         .expect("Something went wrong reading the file");
 
     println!("With text:\n{}", text);
 
+}
+
+struct Config {
+    query: String,
+    file_name: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_name = args[2].clone();
+
+        Config {query, file_name}
+    }
 }
