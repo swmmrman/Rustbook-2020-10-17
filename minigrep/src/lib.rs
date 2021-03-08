a59994224b3d30383273b7b5f96be506af7e04a4
Stephan Kern <stephan@swmmrman.com>
2021-03-08 16:35:37 -0700

    12-20 from book

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -56,4 +56,26 @@ Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+    fn case_insensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
