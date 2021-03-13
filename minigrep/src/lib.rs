9f08f33a345db4d5e4235f4b126029ff32b91ccf
Stephan Kern <stephan@swmmrman.com>
2021-03-12 22:06:59 -0700

    Working CI search.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -23,13 +23,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
-pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+pub fn search_case_insensitive<'a>(
+    query: &str,
+    contents: &'a str
+) -> Vec<&'a str> {
     let mut results = Vec::new();
     let smallq = query.to_lowercase();
 
     for line in contents.lines() {
         let small = line.to_lowercase();
+        if small.contains(&smallq) {
+            results.push(line);
+        }
     }
+    results
 }
 
 pub struct Config {
