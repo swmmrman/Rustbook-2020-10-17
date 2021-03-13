6b363222bbc5dc2858cc8023d86416ba0d30cc04
Stephan Kern <stephan@swmmrman.com>
2021-03-12 22:06:28 -0700

    Fixing the test to match the book.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -71,16 +71,19 @@ Pick three.";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+    #[test]
     fn case_insensitive() {
-        let query = "duct";
+        let query = "rUsT";
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Trust me.";
 
         assert_eq!(
             vec!["Rust:", "Trust me."],
