8db559431ce38600f3bf601052b01bd8d5b3d4f1
Stephan Kern <stephan@swmmrman.com>
2021-03-14 08:37:07 -0600

    CH 13 changes to main. using iterator directly.

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,12 +3,11 @@ use std::{env,process};
 use minigrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("{}", err); //Now i can just print the error here.
         process::exit(1);
     });
+
     println!("Hunting for \"{}\" in {}", config.query, config.file_name);
     if let Err(e) = minigrep::run(config){
         eprint!("File error: {}", e);
