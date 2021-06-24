dada38c2ce10709e7da0ab0d2de1f46e5853f453
Stephan Kern <stephan@swmmrman.com>
2021-06-23 20:41:58 -0600

    Join child thread so it can complete.

--- threads-testing-ch16/src/main.rs
+++ threads-testing-ch16/src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 fn main() {
-    thread::spawn(|| {
+    let th = thread::spawn(|| {
         for i in 0..100 {
             println!("Printing the number {} from the child thread!", i);
             thread::sleep(Duration::from_millis(1));
@@ -12,4 +12,5 @@ fn main() {
         println!("Print the number {} from the parent thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+    th.join().unwrap();
 }
