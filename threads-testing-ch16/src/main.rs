fbf5998d3f827fbd357a32277a44e102bb9dc5fa
Stephan Kern <stephan@swmmrman.com>
2021-06-23 20:40:02 -0600

    playing with threads in ch16,  First ex.

---
+++ threads-testing-ch16/src/main.rs
@@ -0,0 +1,15 @@
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    thread::spawn(|| {
+        for i in 0..100 {
+            println!("Printing the number {} from the child thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    for i in  0..50 {
+        println!("Print the number {} from the parent thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+}
