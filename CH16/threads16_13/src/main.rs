4813c18260dfbeaaa6251376378687053f199219
Stephan Kern <stephan@swmmrman.com>
2021-07-15 20:09:34 -0600

    Generalized the loop, Many errors later, getting a move error.

--- CH16/threads16_13/src/main.rs
+++ CH16/threads16_13/src/main.rs
@@ -12,14 +12,16 @@ fn check_prime(number: i32) -> bool {
 }
 
 fn main() {
-    let pool = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+    let max = 500;
+    let pool: Arc<Vec<i32>> = Arc::new((1..max).collect());
     let index = Arc::new(Mutex::new(0));
     //let primes = Mutex::new(vec![]);
     let mut threads = vec![];
 
-    for _ in 0..20 {
+    for _ in 0..max {
         let index = Arc::clone(&index);
         let handle = thread::spawn(move || {
+            let pool = Arc::clone(&pool);
             let mut cur_index = index.lock().unwrap();
             let num = pool[*cur_index];
             if num % 2 != 0 {
