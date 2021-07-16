0835ecb7871719ecaf611b94b8d37049e31e9e71
Stephan Kern <stephan@swmmrman.com>
2021-07-15 19:50:46 -0600

    Not needed.

--- CH16/Threads16-13/src/main.rs
+++ CH16/Threads16-13/src/main.rs
@@ -14,7 +14,7 @@ fn checkPrime(number: i32) -> bool {
 fn main() {
     let pool = [1,2,3,4,5,6,7,8,9,10];
     let index = Mutex::new(0);
-    let prims = Mutex::new(vec![]);
+    //let primes = Mutex::new(vec![]);
     let mut threads = vec![];
 
     for i in 0..10 {
