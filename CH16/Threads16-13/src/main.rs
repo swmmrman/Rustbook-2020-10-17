ac066970578de5467abacdfd1beac632dc182660
Stephan Kern <stephan@swmmrman.com>
2021-07-15 19:50:17 -0600

    Create a check prime function

--- CH16/Threads16-13/src/main.rs
+++ CH16/Threads16-13/src/main.rs
@@ -1,6 +1,16 @@
 use std::sync::Mutex;
 use std::thread;
 
+fn checkPrime(number: i32) -> bool {
+    let mut prime = true;
+    for i in 3..number+1 {
+        if number / i == 0 {
+            prime = false
+        }
+    }
+    prime
+}
+
 fn main() {
     let pool = [1,2,3,4,5,6,7,8,9,10];
     let index = Mutex::new(0);
