76b674eb94290c2c99490cb6911f8e426436bc4d
Stephan Kern <stephan@swmmrman.com>
2021-07-15 20:08:56 -0600

    Expand range, and fix logic errors

--- CH16/threads16_13/src/main.rs
+++ CH16/threads16_13/src/main.rs
@@ -3,8 +3,8 @@ use std::thread;
 
 fn check_prime(number: i32) -> bool {
     let mut prime = true;
-    for i in 3..number+1 {
-        if number / i == 0 {
+    for i in 3..number {
+        if number % i == 0 {
             prime = false
         }
     }
@@ -12,12 +12,12 @@ fn check_prime(number: i32) -> bool {
 }
 
 fn main() {
-    let pool = [1,2,3,4,5,6,7,8,9,10];
+    let pool = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
     let index = Arc::new(Mutex::new(0));
     //let primes = Mutex::new(vec![]);
     let mut threads = vec![];
 
-    for _ in 0..10 {
+    for _ in 0..20 {
         let index = Arc::clone(&index);
         let handle = thread::spawn(move || {
             let mut cur_index = index.lock().unwrap();
