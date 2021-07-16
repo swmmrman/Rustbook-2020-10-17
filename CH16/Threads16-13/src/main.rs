89417297a5d83cc456e756e002f069dc811c805c
Stephan Kern <stephan@swmmrman.com>
2021-07-15 19:30:15 -0600

    Start loop, and remove uneeded mutex

--- CH16/Threads16-13/src/main.rs
+++ CH16/Threads16-13/src/main.rs
@@ -2,9 +2,17 @@ use std::sync::Mutex;
 use std::thread;
 
 fn main() {
-    let pool = Mutex::new([1,2,3,4,5,6,7,8,9,0]);
+    let pool = [1,2,3,4,5,6,7,8,9,10];
     let index = Mutex::new(0);
     let prims = Mutex::new(vec![]);
     let mut threads = vec![];
-    
+
+    for i in 0..10 {
+        let handle = thread::spawn(move || {
+            let mut curIndex = index.lock().unwrap();
+            
+            *curIndex++;
+
+        })
+    }
 }
