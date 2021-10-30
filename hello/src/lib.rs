29e4727c383546807375e3fe26f8c272f0d4bc30
Stephan Kern <stephan@swmmrman.com>
2021-10-30 14:46:33 -0600

    20-13

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,1 +1,21 @@
 pub struct ThreadPool;
+
+impl ThreadPool {
+    /// Create a new ThreadPool.
+    ///
+    /// The size is the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The new function will panic if the size is 0.
+    pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
+        ThreadPool
+    }
+    pub fn execute<F>(&self, f: F)
+    where
+        F: FnOnce() + Send + 'static,
+        {
+
+        }
+}
