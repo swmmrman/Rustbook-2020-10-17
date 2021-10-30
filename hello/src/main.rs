4d10be2ffcf6bac66b99bc4126a26d3e76087afe
Stephan Kern <stephan@swmmrman.com>
2021-10-30 14:22:26 -0600

    20-10 Lets make a delay

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,4 +1,6 @@
 use std::fs;
+use std::time::Duration;
+use std::thread;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -19,12 +21,17 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
     let get2 = b"GET /css/main.css HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK", "index.html")
     }
     else if buffer.starts_with(get2){
         ("HTTP/1.1 200 OK", "css/main.css")
+    }
+    else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "index.html")
     }
     else {
         ("HTTP/1.1 404 File Not Found", "errors/404.html")
